bf32b131b2ccc29938655d4461000a4e439d4a6e
yiminwook <grs0416@gmail.com>
2024-08-02 09:46:11 +0900

    #DEV#NEW - setup vanilla-extract, storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    vanillaExtractPlugin({
+      identifiers: ({ hash }) => `css_${hash}`,
+    }),
+    svgr(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
